d98941f307b2f4975a2aee354b3dca24287ab052
이승환 <lsh01087674117@gmail.com>
2022-05-28 21:37:22 +0900

    api index title 변경.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'NODE EXPRESS CRUD API' });
 });
 
 module.exports = router;
