02cfe1e331098ced419485b4d931b27b2fef8df9
이승환 <lsh01087674117@gmail.com>
2022-06-02 21:48:39 +0900

    mysql connection 정보추가

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+let connection  = {
+    host: process.env.MariaDB_HOST,
+    port: process.env.MariaDB_PORT,
+    user: process.env.MariaDB_USER,
+    password: process.env.MariaDB_PASS,
+    database: process.env.MariaDB_DATABASE,
+    dateStrings: "date",
+    multipleStatements: true,
+    connectionLimit: 30,
+}
+
+module.exports = mysql.createPool(connection);
