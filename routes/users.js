998a7e14ddeead8455b5c4cf2e65d0b04652dd58
이승환 <lsh01087674117@gmail.com>
2022-06-02 21:53:01 +0900

    getUserList router 추가

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const {getUserList} = require("../service/users");
+
+/**
+ * 유저정보 불러오기
+ */
+router.get('/list', (req, res) => {
+
+    getUserList()
+        .then(result =>
+            res.status(result.status).json(result.value)
+        )
+        .catch(error =>
+            res.status(error.status).json(error.value)
+        );
 });
 
 module.exports = router;
