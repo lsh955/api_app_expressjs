6bc709e8f3bb21b012a098842f1988bdf2a6e8fd
이승환 <lsh01087674117@gmail.com>
2022-06-05 13:03:05 +0900

    기존 함수방식은 삭제하고 formatter 모듈로 리펙토링

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const formatter = require("../util/formatter");
 const {getUserList} = require("../service/users");
+
 
 /**
  * 유저정보 불러오기
  */
-router.get('/list', (req, res) => {
-
-    getUserList()
-        .then(result =>
-            res.status(result.status).json(result.value)
-        )
-        .catch(error =>
-            res.status(error.status).json(error.value)
-        );
+router.get('/list', async (req, res) => {
+    await formatter.routers.responseForm(req, res, getUserList)
 });
 
 module.exports = router;
