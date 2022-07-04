345d572c3528b9d92ef186dc275af206855c3a7c
이승환 <lsh01087674117@gmail.com>
2022-07-04 21:19:42 +0900

    필요 api 추가

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const formatter = require("../util/formatter");
-const {getUserList} = require("../service/users");
+const {
+    UserByAdd,
+    getUserByList,
+    UserByUpdate,
+    UserByDelete
+} = require("../service/users");
+
+/**
+ * 유저정보 추가하기
+ */
+router.post('/', async (req, res) => {
+    const {name, address, date} = req.body;
+
+    UserByAdd(name, address, date)
+        .then(result => formatter.responseForm(result))
+        .catch(error => formatter.responseForm(error))
+});
 
 /**
  * 유저정보 불러오기
@@ -10,7 +26,29 @@ const {getUserList} = require("../service/users");
 router.get('/', async (req, res) => {
     await formatter.validationForm(req)
 
-    getUserList
+    getUserByList
+        .then(result => formatter.responseForm(result))
+        .catch(error => formatter.responseForm(error))
+});
+
+/**
+ * 유저정보 수정하기
+ */
+router.put('/', async (req, res) => {
+    const {name, address, date} = req.body;
+
+    UserByUpdate(name, address, date)
+        .then(result => formatter.responseForm(result))
+        .catch(error => formatter.responseForm(error))
+});
+
+/**
+ * 유저정보 삭제하기
+ */
+router.delete('/', async (req, res) => {
+    const {idx} = req.query;
+
+    UserByDelete(idx)
         .then(result => formatter.responseForm(result))
         .catch(error => formatter.responseForm(error))
 });
