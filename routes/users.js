cc5f2d39592859421fe9a12bd4f435977b5057f1
이승환 <lsh01087674117@gmail.com>
2022-07-09 16:50:35 +0900

    UPDATE, INSERT, DELETE 에 대한 router 추가

--- routes/users.js
+++ routes/users.js
@@ -23,4 +23,43 @@ router.get('/', async (req, res) => {
         .catch(error => responseForm(res, error))
 });
 
+/**
+ * 유저정보 수정하기
+ */
+router.put('/', async (req, res) => {
+    const {idx, name, address, date} = req.query
+
+    await validationForm(req, res)
+
+    await updateByUser(idx, name, address, date)
+        .then(result => responseForm(res, result))
+        .catch(error => responseForm(res, error))
+});
+
+/**
+ * 유저정보 저장하기
+ */
+router.post('/', async (req, res) => {
+    const {name, address, date} = req.body
+
+    await validationForm(req, res)
+
+    await addByUser(name, address, date)
+        .then(result => responseForm(res, result))
+        .catch(error => responseForm(res, error))
+});
+
+/**
+ * 유저정보 삭제하기
+ */
+router.delete('/', async (req, res) => {
+    const {idx} = req.query
+
+    await validationForm(req, res)
+
+    await deleteByUser(idx)
+        .then(result => responseForm(res, result))
+        .catch(error => responseForm(res, error))
+});
+
 module.exports = router;
