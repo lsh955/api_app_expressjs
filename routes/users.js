d6e78688fdd5e1eb47bb0f9841f551f7dad3ea61
이승환 <lsh01087674117@gmail.com>
2022-07-09 09:10:52 +0900

    변수를 엮어서 처리하지 않는방안으로 개선

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const formatter = require("../util/formatter");
+const {validationForm} = require("../util/formatter");
 const {getUserList} = require("../service/users");
 
 /**
  * 유저정보 불러오기
  */
 router.get('/', async (req, res) => {
-    await formatter.validationForm(req)
+    await validationForm(req)
 
     await getUserList()
         .then(result => res.json(result))
