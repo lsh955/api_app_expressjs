3cf5c439e041a049e2b49a5da0023f11741039de
이승환 <lsh01087674117@gmail.com>
2022-07-09 16:50:06 +0900

    UPDATE, INSERT, DELETE 에 대한 모듈추가

--- service/users.js
+++ service/users.js
@@ -20,6 +20,72 @@ const getUserList = async () => {
     }
 }
 
+/**
+ * 유저정보 수정하기
+ * @param idx       시퀀스
+ * @param name      성함
+ * @param address   주소
+ * @param date      등록일시
+ * @returns {Promise<unknown>}
+ */
+const updateByUser = async (idx, name, address, date) => {
+    try {
+        const query = `UPDATE test_data
+                       SET name    = ?,
+                           address = ?,
+                           date    = ?
+                       WHERE idx = ?`;
+
+        const updateData = [name, address, date, idx]
+
+        return await connection(query, updateData);
+    } catch (error) {
+        logFormatter.logs.error(`updateByUser Function Error >> ${error}`)
+        return error
+    }
+}
+
+/**
+ * 유저정보 저장하기
+ * @param name      성함
+ * @param address   주소
+ * @param date      등록일시
+ * @returns {Promise<unknown>}
+ */
+const addByUser = async (name, address, date) => {
+    try {
+        const query = `INSERT INTO test_data (name, address, date)
+                       VALUES (?, ?, ?)`;
+
+        const insertData = [name, address, date]
+
+        return await connection(query, insertData);
+    } catch (error) {
+        logFormatter.logs.error(`addByUser Function Error >> ${error}`)
+        return error
+    }
+}
+
+/**
+ * 유저정보 삭제하기
+ * @param idx       시퀀스
+ * @returns {Promise<unknown>}
+ */
+const deleteByUser = async (idx) => {
+    try {
+        const query = `DELETE
+                       FROM test_data
+                       WHERE idx = ?`;
+
+        const deleteData = [idx]
+
+        return await connection(query, deleteData);
+    } catch (error) {
+        logFormatter.logs.error(`deleteByUser Function Error >> ${error}`)
+        return error
+    }
+}
+
 module.exports = {
     getUserList
 };
